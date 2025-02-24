a444ec8c0b12e52305aa89944ed4de2df9d0503b
Jonathan <electroc_jonathan@hotmail.com>
2025-02-24 14:04:35 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+import './App.css'
+import { FormQuote } from './components/Form'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="title has-text-success has-text-centered">
+        Formulario MissHelados
+      </h1>
+      <div className="container" style={{ width: 600 }}>
+        <div className="columns">
+          <div className="column">
+            <div className="box">
+              <p className="title is-4">Registra tus datos</p>
+              <p className="subtitle is-6">
+              Disfruta con emoción, un helado de fácil digestión.
+              </p>
+              {/* Form */}
+              <FormQuote/>
+              {/* /Form */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,188 @@
+import * as yup from "yup";
+import { Formik, Form, Field } from "formik";
+import { useState } from "react";
+
+const FormQuoteSchema = yup.object().shape({
+  name: yup.string().required("Debe ingresar su nombre."),
+  email: yup
+    .string()
+    .email("Ingrese un correo valido.")
+    .required("Debe ingresar un correo."),
+  quantity: yup.number().moreThan(0).required("Debe especificar la cantidad."),
+  deliveryType: yup.string().required("Debe elegir un tipo de entrega"),
+  tyc: yup.boolean().oneOf([true], "Debe aceptar los términos y condiciones.")
+});
+
+export const FormQuote = () => {
+  const [success, setSuccess] = useState(false);
+
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        quantity: "",
+        deliveryType: "",
+        tyc: false
+      }}
+      validationSchema={FormQuoteSchema}
+      onSubmit={(values, formikProps) => {
+        setTimeout(() => {
+          console.log(values);
+          setSuccess(true);
+          formikProps.resetForm();
+        }, 3000);
+      }}
+    >
+      {({
+        values,
+        handleChange,
+        handleBlur,
+        errors,
+        touched,
+        isSubmitting
+      }) => (
+        <Form>
+          {success && (
+            <div className="notification is-success is-light">
+              <strong>¡Gracias por enviar tu solicitud!</strong> Nuestro equipo
+              se pondrá en contacto contigo pronto.
+            </div>
+          )}
+          <p className="is-4 has-text-grey has-text-weight-bold">
+            Información de contacto
+          </p>
+          <hr className="mt-2" />
+
+          <div className="columns">
+            <div className="column">
+              <div className="field">
+                <label htmlFor="name" className="label">
+                  Nombre
+                </label>
+                <div className="control">
+                  <Field id="name" name="name" type="input" className="input" />
+                  {/* <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    className="input"
+                    value={values.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  /> */}
+                </div>
+                {touched.name && errors.name && (
+                  <p className="help is-danger">{errors.name}</p>
+                )}
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="field">
+                <label htmlFor="email" className="label">
+                  Correo
+                </label>
+                <div className="control">
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    className="input"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </div>
+                {touched.email && errors.email && (
+                  <p className="help is-danger">{errors.email}</p>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <p className="is-4 has-text-grey has-text-weight-bold mb-0 mt-5">
+            Información adicional
+          </p>
+          <hr className="mt-2" />
+          <div className="columns">
+            <div className="column">
+              <div className="field">
+                <label htmlFor="quantity" className="label">
+                  Cantidad de productos
+                </label>
+                <div className="control">
+                  <input
+                    id="quantity"
+                    type="number"
+                    name="quantity"
+                    className="input"
+                    value={values.quantity}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </div>
+                {touched.quantity && errors.quantity && (
+                  <p className="help is-danger">{errors.quantity}</p>
+                )}
+              </div>
+            </div>
+            <div className="column">
+              <div className="field">
+                <label htmlFor="deliveryType" className="label">
+                  Tipo de entrega
+                </label>
+                <div className="select">
+                  <Field id="deliveryType" name="deliveryType" as="select">
+                    <option value="">Seleccionar</option>
+                    <option value="delivery">Delivery</option>
+                    <option value="pickup">Recojo en tienda</option>
+                  </Field>
+                  {/* <select
+                    id="deliveryType"
+                    value={values.deliveryType}
+                    name="deliveryType"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  >
+                    <option value="">Seleccionar</option>
+                    <option value="delivery">Delivery</option>
+                    <option value="pickup">Recojo en tienda</option>
+                  </select> */}
+                </div>
+                {touched.deliveryType && errors.deliveryType && (
+                  <p className="help is-danger">{errors.deliveryType}</p>
+                )}
+              </div>
+            </div>
+          </div>
+          <hr className="mt-2" />
+
+          <label className="checkbox">
+            <input
+              type="checkbox"
+              name="tyc"
+              checked={values.tyc}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />{" "}
+            Acepto los términos y condiciones
+            {touched.tyc && errors.tyc && (
+              <p className="help is-danger">{errors.tyc}</p>
+            )}
+          </label>
+          <div className="mt-4 is-flex is-justify-content-flex-end">
+            <button
+              className={`button is-success ${
+                isSubmitting ? "is-loading" : ""
+              }`.trim()}
+              type="submit"
+            >
+              Enviar cotización
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
