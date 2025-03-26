8ce4a0670165e13d6289eee85df5914fc237cf3d
Jonathan <electroc_jonathan@hotmail.com>
2025-03-25 18:29:43 -0600

    update headers

--- src/handleSubmit.jsx
+++ src/handleSubmit.jsx
@@ -1,10 +1,41 @@
+// import axios from "axios";
+
+// export const handleSubmit = async (values, formikProps, setSuccess,setIsSubmitting, recaptchaRef) => {
+//     console.log(values);
+//     try {
+//         //const response = await axios.post("http://localhost:8080/api/cliente", values);
+//         const response = await axios.post("https://webmicfx.arashi.solutions/FGR/WsAjaxCoClien", values);
+//         console.log("Datos guardados:", response.data);
+//         setSuccess(true);
+//         formikProps.resetForm();
+//         recaptchaRef.current.reset();
+        
+//     } catch (error) {
+//         console.error("Error al guardar los datos:", error);
+//     } finally{
+//         setIsSubmitting(false);
+//     }
+// };
+
 import axios from "axios";
 
-export const handleSubmit = async (values, formikProps, setSuccess,setIsSubmitting, recaptchaRef) => {
+export const handleSubmit = async (values, formikProps, setSuccess, setIsSubmitting, recaptchaRef) => {
     console.log(values);
     try {
-        //const response = await axios.post("http://localhost:8080/api/cliente", values);
-        const response = await axios.post("https://webmicfx.arashi.solutions/FGR/WsAjaxCoClien", values);
+        const response = await axios.post(
+            "https://webmicfx.arashi.solutions/FGR/WsAjaxCoClien", 
+            JSON.stringify(values), // Convertir a JSON
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                mode: "cors" // Asegurar que la solicitud use CORS
+
+                
+            }
+        );
+
         console.log("Datos guardados:", response.data);
         setSuccess(true);
         formikProps.resetForm();
@@ -12,7 +43,8 @@ export const handleSubmit = async (values, formikProps, setSuccess,setIsSubmitti
         
     } catch (error) {
         console.error("Error al guardar los datos:", error);
-    } finally{
+    } finally {
         setIsSubmitting(false);
     }
 };
+
